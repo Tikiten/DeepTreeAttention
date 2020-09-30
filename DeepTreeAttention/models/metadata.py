12ad0008294596aee8b3a4161f27808851560ebe
bw4sz <benweinstein2010@gmail.com>
2020-09-30 14:57:03 -0700

    random shuffle

--- DeepTreeAttention/models/metadata.py
+++ DeepTreeAttention/models/metadata.py
@@ -5,8 +5,8 @@ from tensorflow.keras.layers import Dense, Input
 def metadata_model(classes):
     # create model
     metadata_inputs = Input(shape=(1,))
-    metadata_inputs = tf.random.shuffle(metadata_inputs)    
-    x = Dense(classes*2, activation='relu')(metadata_inputs)
+    metadata = tf.random.shuffle(metadata_inputs)    
+    x = Dense(classes*2, activation='relu')(metadata)
     x = Dense(classes, activation='relu')(x)
     output = Dense(classes, activation="softmax")(x)
     
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -6,15 +6,14 @@ import tensorflow as tf
 
 @pytest.fixture()
 def sites():
-    metadata = np.zeros((1,23))
-    metadata[0,10] = 1
-    metadata
+    metadata = np.zeros((1,1))
+    #metadata[0,10] = 1
     return metadata
 
 #Test full model makes the correct number of predictions.
 def test_model(sites):
     classes=74
-    inputs, outputs= metadata_model(classes=classes, sites=len(sites[0,]))    
+    inputs, outputs= metadata_model(classes=classes)    
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     prediction = model.predict(sites)
     
